62a75a93be9a6f40e5f7759500863ad17712b53f
ALEJANDRO GONZÁLEZ SÁNCHEZ <Alejandro.Gonzalez41@alu.uclm.es>
2024-11-07 19:50:58 +0100

    Renombrada la variable del loop de buscaAgentes. Implementado el ID de agente automático según el tiempo de creación. Implementada la función de anidamiento de agente en un puerto.

--- cambiaCromosProyecto/src/pk/Agent.java
+++ cambiaCromosProyecto/src/pk/Agent.java
@@ -9,30 +9,53 @@ import java.util.List;
 public class Agent{
 
     // Constructor
-    public Agent(int id, InetAddress control, int controlPort) throws IOException {
-        this.id = id; // Asignada en construcción de objeto, se puede plantear asignacón automática
+    public Agent(InetAddress control, int controlPort) throws IOException {
+        long time = System.currentTimeMillis();
+        this.id = String.valueOf(time) ; // El ID será el tiempo de creación de objeto.
         this.dir = InetAddress.getLocalHost();
-        this.listenSocket = new ServerSocket(0);  //port = 0 => busca automáticamente un puerto disponible
-        this.port = this.listenSocket.getLocalPort();
+        this.listenSocket = new ServerSocket();  //port = 0 => busca automáticamente un puerto disponible
+        this.portMin = 4000;
+        this.portMax = 4100;
         this.controlDir = control;
         this.controlPort = controlPort;
         this.listaIPs = new ArrayList<>();
-        assignSubnetIPs();
+        findDir();          // Se coloca en un socket del rango establecido
+        assignSubnetIPs();  // Define la lista de IP de nuestra subred.
     }
 
     // Lista de atributos
-    public int id;                      // Id del agente
+    private String id;                      // Id del agente
     private ServerSocket listenSocket;  // Socket de escucha
     private int port;                   // Puerto del socket de escucha en la máquina
+    private int portMin = 4000;        // Comienzo del rango de puertos que usaremos para agentes
+    private int portMax = 4100;        // Fin del rango de puertos que usaremos para agentes
     private InetAddress dir;            // Dirección del agente
     private InetAddress controlDir;     // Dirección del controlador a cargo del agente
     private int controlPort;            // Número de puerto para conectarnos al controlador
     private List<String> listaIPs;
 
+
     // Getters
     public InetAddress getDir() {return dir;}
     public ServerSocket getSocket() {return this.listenSocket;}
     public int getPort(){return this.port;}
+
+    private void findDir() throws UnknownHostException {
+        String ipAdd = InetAddress.getLocalHost().getHostAddress();
+        System.out.println("IP Address of the machine: " + ipAdd);
+        for(int p = portMin; p <= portMax; p += 2){
+            try{
+                System.out.println("Attempting to nest in port " + p);
+                InetSocketAddress address = new InetSocketAddress(ipAdd, p);
+                listenSocket.bind(address);
+                this.port = p;
+                break;
+            } catch (IOException e) {
+                System.out.println("Couldn't nest in port.");
+            }
+        }
+        System.out.println("Successfully nested in port "+ port);
+    }
 
     private void assignSubnetIPs() throws IOException {
         String localIp = dir.getHostAddress(); // Get local IP as a string
@@ -51,7 +74,7 @@ public class Agent{
     }
 
     // Método de escucha que lanza un thread para cada mensaje recibido
-    public void listen() {
+    private void listen() {
         System.out.println("Agente en escucha en el puerto " + port + "...");
         try {
             while (true) {  // Bucle de escucha infinito
@@ -93,7 +116,7 @@ public class Agent{
         System.out.println("Agente creado en proceso con PID:"+ProcessHandle.current().pid());
         System.out.flush();
 
-        Agent agent = new Agent(1, InetAddress.getLocalHost(), 2);
+        Agent agent = new Agent(InetAddress.getLocalHost(), 2);
         System.out.println("Agente "+agent.id+", con dirección "+agent.getDir().toString()+" y puerto "+agent.getPort());
         System.out.flush();
 
@@ -120,7 +143,7 @@ public class Agent{
                 for (String ipString : listaIPs) {  // iterar las ips en la lista de ips
                     try {
                         InetAddress ipAddress = InetAddress.getByName(ipString);
-                        for (int port = 4000; port <= 4100; port += 2) {  // Iterar puertos pares dentro del rango
+                        for (int p = portMin; port <= portMax; p += 2) {  // Iterar puertos pares dentro del rango
 
                             // Espera de 2 segundos entre comprobaciones
                             //try {
@@ -131,14 +154,14 @@ public class Agent{
                             //    break;
                             //}
 
-                            System.out.println("Agente buscando en IP: " + ipString + ", Puerto: " + port);
+                            System.out.println("Agente buscando en IP: " + ipString + ", Puerto: " + p);
                             try{
                                 // Intenta conectarse al supuesto agente localizado en ip y puerto
                                 Socket agentSocket = new Socket();
-                                InetSocketAddress address = new InetSocketAddress(ipString, port);                                // Reducimos el timeout para que tarde menos en iterar sobre los puertos
+                                InetSocketAddress address = new InetSocketAddress(ipString, p);                                // Reducimos el timeout para que tarde menos en iterar sobre los puertos
                                 agentSocket.connect(address, 1);
 
-                                System.out.println("Agente encontrado en IP: " + ipString + ", Puerto: " + port);
+                                System.out.println("Agente encontrado en IP: " + ipString + ", Puerto: " + p);
                                 // Mandar el mensaje de hola al agente
                                 PrintWriter out = new PrintWriter(agentSocket.getOutputStream(), true);
                                 //TODO: cambiar formato del mensaje cuando lo tengamos.
@@ -155,7 +178,7 @@ public class Agent{
                                 }
                             } catch (IOException e) {
                                 // Errores de conexión si un agente no está disponible
-                                System.out.println("No se pudo conectar a " + ipString + " en el puerto " + port);
+                                System.out.println("No se pudo conectar a " + ipString + " en el puerto " + p);
                             }
                         }
                     } catch (UnknownHostException e) {
