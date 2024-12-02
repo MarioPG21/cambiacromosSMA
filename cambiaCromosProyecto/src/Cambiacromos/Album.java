a401116be7d85be82347ade63503aeee4a6aae09
MARIO PAÑOS GIRALDO <Mario.Panos2@alu.uclm.es>
2024-12-02 19:40:09 +0100

    Abrir sobre inicial completado

--- cambiaCromosProyecto/src/Cambiacromos/Album.java
+++ cambiaCromosProyecto/src/Cambiacromos/Album.java
@@ -1,40 +1,109 @@
 package Cambiacromos;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Random;
+
+// En la colección hay 100 cromos en total. Cada cromo tiene un valor distinto, una probabilidad de aparición en el set de inicio y pertenece a un set distinto.  Los valores deben poder ser fácilmente modificados.
+
+// En la colección hay los siguientes sets: 5 de 10 cartas, que serán sets básicos y aportarán un valor adicional fácilmente modificable. 5 de 10 cartas que serán los raros y que aportarán un valor fácilmente modificable.
 
 // TODO: Sobre de inicio
-// Dentro del álbum se definen los 100 cromos. Una función en el constructor rellena el álbum con unas cartas iniciales.
+// Se debe crear una lista de los 100 cromos distintas con sus valores asignados. Una función en el constructor rellena el álbum con 50 cromos iniciales, obtenidos aleatoriamente en base a su probabilidad de aparición.
 
 // TODO: Definir sets
-// Definir los sets del álbum y aplicar su valor adicional al mismo.
+// Se debe aplicar el valor adicional de un set al álbum.
 
 // TODO: Orden de las listas de deseados y ofrecidos
 
-// TODO: actualización del valor del álbum
+// TODO: Actualización del valor del álbum
+
 
 
 public class Album {
-    private List<Cromo> coleccion;
-    private List<Cromo> lista_deseados;
-    private List<Cromo> lista_ofrezco;
-    private double valorTotal;
+    private static final List<Cromo> COLECCION = generarCromos();
+
+    private List<Cromo> tengo; // lista de cromos
+    private List<Cromo> lista_deseados; // lista de los 10 cromos que no forman parte de la colección y que nos aportarían un valor total mayor al actual.
+    private List<Cromo> lista_ofrezco; // lista de los 10 cromos que forman parte de la colección que menos valor total nos harían perder si faltasen.
+    private double valorTotal; // valor de cada carta en la lista de cromos a demás del valor de los sets completados
 
     public Album() {
-        this.coleccion = new ArrayList<>();
+        this.tengo = new ArrayList<>();
         this.lista_deseados = new ArrayList<>();
         this.lista_ofrezco = new ArrayList<>();
         this.valorTotal = 0;
+        abrirSobreInicial();
+    }
+
+    private static List<Cromo> generarCromos() {
+        List<Cromo> cromos = new ArrayList<>();
+        List<Double> probabilidades = new ArrayList<>();
+
+        // Las primeras 50 cartas tienen una probabilidad de 15% de aparecer cada una
+        for (int i = 0; i < 50; i++) {
+            probabilidades.add(0.15);
+        }
+        // Las siguientes 50 cartas tienen una probabilidad de 5% de aparecer cada una
+        for (int i = 50; i < 100; i++) {
+            probabilidades.add(0.05);
+        }
+
+
+        // Para los primeros 5 sets...
+        for(int i = 1; i<=5; i++){
+            // Cada carta del set...
+            for(int j = 1; j<=10;j++){
+                int valor = 1;
+                int id = ((i-1)*10) + j;
+                // Tiene un valor de 1, pertenece a los set [1-5] y tiene como id [1-50]
+                cromos.add(new Cromo(id,valor, probabilidades.get(id-1),i));
+            }
+        }
+        // Para los siguientes 5 sets...
+        for(int i = 6; i<=10; i++){
+            // Cada carta del set...
+            for(int j = 1; j<=10;j++){
+                int valor = 1;
+                int id = ((i-1)*10) + j;
+                // Tiene un valor de 1, pertenece a los set [6-10] y tiene como id [51-100]
+                cromos.add(new Cromo(id,valor, probabilidades.get(id-1),i));
+            }
+        }
+
+        return cromos;
+    }
+
+    private void abrirSobreInicial() {
+        for (int n = 0; n < 51; n++) {
+            // Lista de probabilidades acumuladas. Convierte las probabilidades individuales en un rango, formado entre su propia probabilidad y la anterior.
+            // Por ejemplo, si la probabilidad de las dos primeras cartas es 1%, p_acumuladas[0] = 1 y p_acumuladas[1] = 2.
+            double[] p_acumuladas = new double[COLECCION.size()];
+            p_acumuladas[0] = COLECCION.get(0).getProbabilidad();
+
+            for (int i = 1; i < COLECCION.size(); i++) {
+                p_acumuladas[i] = p_acumuladas[i - 1] + COLECCION.get(i).getProbabilidad();
+            }
+
+            // Generar un número aleatorio entre 0 y el total de probabilidades
+            double aleatorio = Math.random() * p_acumuladas[p_acumuladas.length - 1];
+
+            // Encontrar el cromo correspondiente
+            for (int i = 0; i < p_acumuladas.length; i++) {
+                if (aleatorio <= p_acumuladas[i]) {
+                    this.tengo.add(COLECCION.get(i));
+                    break;
+                }
+            }
+        }
     }
 
     public double getValorTotal() {
         return valorTotal;
     }
 
-    public List<Cromo> getColeccion() {
-        return coleccion;
+    public List<Cromo> getTengo() {
+        return tengo;
     }
 
     public List<Cromo> getLista_deseados() {
@@ -43,5 +112,17 @@ public class Album {
 
     public List<Cromo> getLista_ofrezco() {
         return lista_ofrezco;
+    }
+
+    @Override
+    public String toString() {
+        return "Album{" +
+                "tengo=" + tengo +
+                ", valorTotal=" + valorTotal +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        System.out.printf(new Album().toString());
     }
 }
--- cambiaCromosProyecto/src/Cambiacromos/Cromo.java
+++ cambiaCromosProyecto/src/Cambiacromos/Cromo.java
@@ -2,28 +2,42 @@ package Cambiacromos;
 
 public class Cromo {
     private int id; // Entre 1 y 100
-    private double valor;
-    private double probabilidad;
+    private int valor; // valor de una carta por su rareza
+    private Double probabilidad; // probabilidad de aparición
+    private int set; // Conjunto de cartas dentro de la colección al que pertenece. Cuando se tienen las 5 cartas de un mismo set, el álbum obtiene un valor adicional.
 
-    public Cromo(int id, int valor, int probabilidad) {
-        if (id < 1 || id > 100) {
-            throw new IllegalArgumentException("El ID debe estar entre 1 y 100.");
-        }
+    public Cromo(int id, int valor, Double probabilidad, int set) {
         this.id = id;
         this.valor = valor;
         this.probabilidad = probabilidad;
+        this.set = set;
     }
 
     public int getId() {
         return id;
     }
 
-    public double getValor() {
+    public int getValor() {
         return valor;
     }
 
-    public double getProbabilidad() {
+    public Double getProbabilidad() {
         return probabilidad;
+    }
+
+    public int getSet() {
+        return this.set;
+    }
+
+    @Override
+    public String toString() {
+        return ""+id;
+        /*return "Cromo{" +
+                "id=" + id +
+                ", valor=" + valor +
+                ", probabilidad=" + probabilidad +
+                ", set=" + set +
+                '}';*/
     }
 }
 
